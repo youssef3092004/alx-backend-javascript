0788698725c630c808655bab2ef8baa3e5ceaab0
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-02 20:53:57 +0300

    task 4

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
     .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
