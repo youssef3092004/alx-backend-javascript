84bf39ad3d8b463dbd5396fa3b127061fb97bf25
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-04 00:27:13 +0300

    feat: implement HolbertonClass with size and location attributes
    - Add constructor to initialize size and location
    - Implement valueOf() method to return size when cast to Number
    - Implement toString() method to return location when cast to String
    - Add getters and setters for size and location with validation

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,35 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(newSize) {
+    if (typeof newSize === 'number') {
+      this._size = newSize;
+    } else {
+      throw new Error('Size must be a number');
+    }
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(newLocation) {
+    if (typeof newLocation === 'string') this._location = newLocation;
+    else throw new Error('Location must be a string');
+  }
+
+  valueof() {
+    return this.size;
+  }
+
+  tostring() {
+    return this.location;
+  }
+}
