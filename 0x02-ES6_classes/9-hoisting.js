199c355d0f4348de0d10220b8212a8a4c60aaa27
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-04 01:00:35 +0300

    fix: correct class definitions, property assignments, and method implementations in HolbertonClass and StudentHolberton

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -13,11 +13,14 @@ export class HolbertonClass {
     }
   }
   
+  const class2019 = new HolbertonClass(2019, 'San Francisco');
+  const class2020 = new HolbertonClass(2020, 'San Francisco');
+  
   export class StudentHolberton {
     constructor(firstName, lastName, holbertonClass) {
       this._firstName = firstName;
       this._lastName = lastName;
-      this._holbertonClass = holbertonClass;
+      this._holbertonClass = holbertonClass; // Use the parameter passed to the constructor
     }
   
     get fullName() {
@@ -25,24 +28,20 @@ export class HolbertonClass {
     }
   
     get holbertonClass() {
-      return this._holbertonClass;
+      return this._holbertonClass; // Corrected to return the instance variable
     }
   
     get fullStudentDescription() {
-      return `${this._firstName} ${this._lastName} - \
-  ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+      return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
     }
   }
   
-  const class2019 = new HolbertonClass(2019, 'San Francisco');
-  const class2020 = new HolbertonClass(2020, 'San Francisco');
-  
+  // Creating student instances after the class is defined
   const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
   const student2 = new StudentHolberton('John', 'Doe', class2020);
   const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
   const student4 = new StudentHolberton('Donald', 'Bush', class2019);
   const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
   
-  const listOfStudents = [student1, student2, student3, student4, student5];
+  export const listOfStudents = [student1, student2, student3, student4, student5];
   
-  export default listOfStudents;
