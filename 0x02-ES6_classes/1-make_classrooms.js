83530e92aeeca2448194bab29e8c071bfd0b5f8c
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-03 22:17:41 +0300

    task 2

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,24 +1,25 @@
-// Import the Classroom class from the specified path
-// The Classroom class is expected to be located in the "0x02-ES6_classes" directory,
-// and is defined in the "0-classroom.js" file.
-import Classroom from './0-classroom';
+// Import the ClassRoom class from the specified path
+// The ClassRoom class is expected to be located in the "0x02-ES6_classes" directory,
+// and is defined in the "0-ClassRoom.js" file.
+import ClassRoom from './0-Classroom';
 
 /**
  * Function: initializeRooms
  *
- * This function creates an array of three Classroom objects, each initialized with a different
+ * This function creates an array of three ClassRoom objects, each initialized with a different
  * maximum student size (19, 15, and 10 respectively).
  *
- * @returns {Array} - An array of Classroom objects
+ * @returns {Array} - An array of ClassRoom objects
  *
  * Example output:
  * [
- *   Classroom { _maxStudentsSize: 19 },
- *   Classroom { _maxStudentsSize: 15 },
- *   Classroom { _maxStudentsSize: 10 }
+ *   ClassRoom { _maxStudentsSize: 19 },
+ *   ClassRoom { _maxStudentsSize: 15 },
+ *   ClassRoom { _maxStudentsSize: 10 }
  * ]
  */
 export default function initializeRooms() {
-  // Create an array with three Classroom instances, each with the specified size
+  // Create an array with three ClassRoom instances, each with the specified size
   return [19, 20, 34].map((size) => new ClassRoom(size));
+
 }
