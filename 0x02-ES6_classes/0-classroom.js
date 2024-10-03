647f0b9afc99c53c437f612712e24640aab510a7
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-03 21:58:28 +0300

    UPDATE: make a betty for task 1

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,11 +2,12 @@
  * Represents a classroom.
  */
 export default class Classroom {
-    /**
+  /**
      * Creates a new Classroom.
-     * @param {number} maxStudentsSize - The maximum number of students that can be accommodated in the classroom.
+     * @param {number} maxStudentsSize - The maximum number of students that can be
+     * accommodated in the classroom.
      */
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize  = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
