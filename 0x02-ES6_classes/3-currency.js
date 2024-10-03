00de41d740ef3c04d596f260ca49eec20560bd3d
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-03 23:11:53 +0300

    UPDATE: make a betty for the code use eslint

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,32 +1,28 @@
 export default class Currency {
-    constructor(code, name) {
-        this.code = code;
-        this.name = name;
-    }
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
 
-    get code() {
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set code(newCode) {
-        if (typeof newCode === 'string')
-            this._code = newCode;
-        else
-            throw new Error('Code must be a string');
-    }
+  set code(newCode) {
+    if (typeof newCode === 'string') this._code = newCode;
+    else throw new Error('Code must be a string');
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(newName) {
-        if (typeof newName === 'string')
-            this._name = newName;
-        else
-            throw new Error('Name must be a string');
-    }
+  set name(newName) {
+    if (typeof newName === 'string') this._name = newName;
+    else throw new Error('Name must be a string');
+  }
 
-    displayFullCurrency() {
-        return `${this.name} (${this.code})`;
-    }
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
