28373a37a64a936ba0757f471e843117d48a5a93
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-03 22:51:28 +0300

    task 3

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constractor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this.length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'string') {
+      this._length = newLength;
+    } else {
+      throw new TypeError('Length must be a string');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents) && newStudents.every((student) => typeof student === 'string')) {
+      this._students = newStudents;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+}
