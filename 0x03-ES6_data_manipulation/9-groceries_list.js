ed1f7abec267f41e6af565577fd0b4ccb3b95327
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-09 03:54:02 +0300

    FIX: Implement groceriesList function to return a Map of grocery items with their quantities

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
 export default function groceriesList() {
-    value = [
-        ['Apples', '10'],
-        ['Tomatoes', '10'],
-        ['Pasta', '1'],
-        ['Rice', '1'],
-        ['Banana', '5']
-    ]
-    return new Map(value);
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
 }
