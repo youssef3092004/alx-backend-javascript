ab1667781f395f2de8e442bdda5ca346f6d73c15
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-09 03:03:42 +0300

    ADD:Implement createInt8TypedArray function to create a DataView and set a value

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,7 @@
+export default function createInt8TypedArray(length, position, value) {
+    if (position >= length) throw new Error('Position outside range');
+    
+    const buffer = new DataView(new ArrayBuffer(length));
+    buffer.setInt8(position, value);
+    return buffer;
+}
